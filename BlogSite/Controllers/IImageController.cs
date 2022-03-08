12f4f8911b9a863db589297c465cc0b84d5cd6cb
GT3CH1 <gavinpease@gmail.com>
2022-03-08 12:41:33 -0700

    Enable image uploading

--- BlogSite/Controllers/IImageController.cs
+++
@@ -1,8 +0,0 @@
-using Microsoft.AspNetCore.Mvc;
-
-namespace BlogSite.Controllers;
-
-public interface IImageController
-{
-    public IActionResult Upload();
-}
--- BlogSite/Controllers/PostsController.cs
+++ BlogSite/Controllers/PostsController.cs
@@ -1,6 +1,12 @@
-﻿using BlogSite.Models;
+﻿using System.Buffers.Text;
+using System.Text.Json.Nodes;
+using System.Text.Json.Serialization;
+using BlogSite.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Serialization;
 
 namespace BlogSite.Controllers;
 
@@ -18,12 +24,28 @@ public class PostsController : Controller, IPostController
     {
         return View();
     }
-    
+
     [Route("/Posts/View/{postId}")]
     public IActionResult PostsView(int postId)
     {
         PostModel post = new PostModel(postId);
         ViewBag.Message = post;
         return View();
+    }
+
+    [Route("/Posts/UploadImage")]
+    [HttpPost]
+    [Authorize(Roles = "Admin")]
+    public string UploadImage(string fileData, string fileName)
+    {
+        // FileObject fo = JsonConvert.DeserializeObject<FileObject>(fileObject);
+        //TODO: Parse fileName and save fileData to somewhere secure.
+        return JsonConvert.SerializeObject(new Location());
+    }
+
+    public class Location
+    {
+        [JsonProperty(PropertyName = "location")]
+        public string location = "/change/me";
     }
 }
