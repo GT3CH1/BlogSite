93095822caddef0667c8c5efbe22c0ef1c753e77
Gavin <gavinpease@gmail.com>
2022-03-08 15:36:13 -0700

    Create a photoservice
    Signed-off-by: Gavin <gavinpease@gmail.com>

---
+++ BlogSite/Interfaces/IPhotoService.cs
@@ -0,0 +1,43 @@
+﻿namespace BlogSite.Services;
+
+public interface IPhotoService
+{
+    private static Random random = new Random();
+
+    /// <summary>
+    /// Uploads the given byte array (image).
+    /// </summary>
+    /// <param name="base64"></param>
+    /// <returns></returns>
+    public bool Upload(string fileName, string base64, out string path);
+
+    /// <summary>
+    /// Checks whether or not the given filename exists.
+    /// </summary>
+    /// <param name="filename"></param>
+    /// <returns></returns>
+    public bool ImageExists(string filename);
+
+    /// <summary>
+    /// Creates an alternative file name if there is a collision.
+    /// </summary>
+    /// <param name="filename"></param>
+    /// <returns></returns>
+    public string GetAlternativeFilename(string filename);
+
+    /// <summary>
+    /// Gets the filepath/name of an uploaded image.
+    /// </summary>
+    /// <param name="filename"></param>
+    /// <returns></returns>
+    public string GetNewImageName(string filename);
+
+    public string GetImage(string imageName);
+
+    public static string RandomString()
+    {
+        const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_";
+        return new string(Enumerable.Repeat(chars, 8)
+            .Select(s => s[random.Next(s.Length)]).ToArray());
+    }
+}
---
+++ BlogSite/Services/PhotoService.cs
@@ -0,0 +1,75 @@
+﻿namespace BlogSite.Services;
+
+public class PhotoService : IPhotoService
+{
+    /// <summary>
+    /// The media directory to write to - INCLUDING TRAILING SLASH
+    /// </summary>
+    public readonly string MEDIA_DIR = "./Media/";
+
+    public readonly string[] ALLOWED_EXTENSIONS = { ".jpg", ".png", ".gif", ".webp" };
+
+    public bool Upload(string base64, string fileName, out string filePath)
+    {
+        byte[] bytes = Convert.FromBase64String(base64);
+        var extension = Path.GetExtension(fileName);
+
+        if (!ALLOWED_EXTENSIONS.Contains(extension))
+        {
+            filePath = "";
+            return false;
+        }
+
+        fileName = fileName.Replace(" ", "_");
+        if (!Directory.Exists(MEDIA_DIR))
+            Directory.CreateDirectory(MEDIA_DIR);
+        filePath = GetNewImageName(fileName);
+        File.WriteAllBytes($"{MEDIA_DIR}{filePath}", bytes);
+        filePath = $"/Image/Get/{filePath}";
+        return true;
+    }
+
+    public bool ImageExists(string filename)
+    {
+        var fileExists = File.Exists(filename);
+        if (!fileExists)
+            fileExists = File.Exists($"{MEDIA_DIR}{filename}");
+        return fileExists;
+    }
+
+    public string GetPathIfImageExists(string filename)
+    {
+        var path = filename;
+        if (!File.Exists(filename))
+            path = $"{MEDIA_DIR}{filename}";
+        if (!File.Exists(path))
+            return "";
+        return $"/Media/{filename}";
+    }
+
+    public string GetAlternativeFilename(string filename)
+    {
+        var fileName = Path.GetFileNameWithoutExtension($"{MEDIA_DIR}{filename}");
+        var extension = Path.GetExtension(filename);
+        return $"{fileName}_{IPhotoService.RandomString()}{extension}";
+    }
+
+    public string GetNewImageName(string filename)
+    {
+        if (ImageExists(filename))
+        {
+            var imageExists = ImageExists(GetAlternativeFilename(filename));
+            while (imageExists)
+                imageExists = ImageExists(GetAlternativeFilename(filename));
+            return $"{GetAlternativeFilename(filename)}";
+        }
+
+        return $"{filename}";
+    }
+
+    public string GetImage(string imageName)
+    {
+        var img = File.ReadAllBytes($"{MEDIA_DIR}{imageName}");
+        return Convert.ToBase64String(img);
+    }
+}
