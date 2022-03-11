9226eca93553fc8811ec2023f93a14d1423e3949
GT3CH1 <gavinpease@gmail.com>
2022-03-11 07:49:18 -0700

    remove script, onload, and onerror
    Signed-off-by: GT3CH1 <gavinpease@gmail.com>

--- BlogSite/Models/PostModel.cs
+++ BlogSite/Models/PostModel.cs
@@ -1,16 +1,44 @@
 ﻿using BlogSite.Data;
+using BlogSite.Interfaces;
 
 namespace BlogSite.Models;
 
-public class PostModel
+public class PostModel : IPostModel
 {
     public int Id { get; set; } = -1;
-    public string Title { get; private set; }
-    public string Content { get; private set; }
 
-    public PostModel()
+    /// <summary>
+    /// The title, without any script tags.
+    /// </summary>
+    private string _title = string.Empty;
+
+    public string Title
     {
-        
+        get => _title;
+        set
+        {
+            _title = value;
+            _title = _title.Replace("script", "");
+            _title = _title.Replace("onload", "");
+            _title = _title.Replace("onerror", "");
+        }
+    }
+
+    /// <summary>
+    /// The content without any script tags.
+    /// </summary>
+    private string _content = string.Empty;
+
+    public string Content
+    {
+        get => _content;
+        set
+        {
+            _content = value;
+            _content = _content.Replace("script", "");
+            _title = _title.Replace("onload", "");
+            _title = _title.Replace("onerror", "");
+        }
     }
 
     public PostModel(string title, string content, int id)
