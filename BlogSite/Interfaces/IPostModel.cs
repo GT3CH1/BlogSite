ff7dbfde10f45d23df94e371d68212c2ae564674
GT3CH1 <gavinpease@gmail.com>
2022-03-10 19:10:30 -0700

    Add interface for PostModel
    Signed-off-by: GT3CH1 <gavinpease@gmail.com>

---
+++ BlogSite/Interfaces/IPostModel.cs
@@ -0,0 +1,34 @@
+namespace BlogSite.Interfaces;
+
+public interface IPostModel
+{
+    /// <summary>
+    /// The ID of a post.
+    /// </summary>
+    public int Id { get; set; }
+    
+    /// <summary>
+    /// The title of a post.
+    /// </summary>
+    public string Title { get; set; }
+    
+    /// <summary>
+    /// The contents of a post.
+    /// </summary>
+    public string Content { get; set; }
+    
+    /// <summary>
+    /// Deletes this post from the database.
+    /// </summary>
+    public void Delete();
+    
+    /// <summary>
+    /// Updates this post, by setting the post title and content.
+    /// </summary>
+    public void Update();
+    
+    /// <summary>
+    /// Creates this post in the database.
+    /// </summary>
+    public void Create();
+}
