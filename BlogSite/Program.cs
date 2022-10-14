bd2edbdfc75809922b5471351f2807725e24a232
Gavin Pease <gavinpease@gmail.com>
2022-10-13 20:38:36 -0600

    move connection strings from appsettings.json to Development

--- BlogSite/Program.cs
+++ BlogSite/Program.cs
@@ -53,14 +53,15 @@ using (var scope = app.Services.CreateScope())
 if (app.Environment.IsDevelopment())
 {
     app.UseMigrationsEndPoint();
+    Console.WriteLine("Development");
 }
 else
 {
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+    Console.WriteLine("Production");
 }
-
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
